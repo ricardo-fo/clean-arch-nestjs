0eb39f2063a02fe75282bee6a98533b1660f916c
Ricardo Oliveira <ricardo-cadastros@hotmail.com>
2023-12-20 20:03:21 -0300

    [feature] Cria estrutura de arquivos.

---
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -0,0 +1,16 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { EnvConfigService } from './env-config.service';
+import { ConfigModule, ConfigModuleOptions, ConfigService } from '@nestjs/config';
+import { join } from 'node:path';
+
+@Module({
+  providers: [EnvConfigService, ConfigService]
+})
+export class EnvConfigModule extends ConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return super.forRoot({
+      ...options,
+      envFilePath: [join(__dirname, `../../../../.env.${process.env.NODE_ENV}`)]
+    });
+  }
+}
