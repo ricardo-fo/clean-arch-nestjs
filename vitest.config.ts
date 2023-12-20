8139e1f9c7aae7a52a02159d68f437eabaaa009a
Ricardo Oliveira <ricardo-cadastros@hotmail.com>
2023-12-20 20:47:23 -0300

    [feature] Implementa vitest.

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    include: ['**/*.spec.ts'],
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    }
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+});
